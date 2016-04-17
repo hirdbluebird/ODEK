1efe6f0ec4d23d8d1304abf20b0eab339961b0b0
hirdbluebird <bluebirdyourdestiny@gmail.com>
2016-04-17 20:13:48 +0300

    commit before database dump

--- footer.php
+++ footer.php
@@ -10,5 +10,6 @@
     <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/all.js"></script>
   </body>
 </html>
