bc6264a27a409766221215fe1215ef6695f05d70
hirdbluebird <bluebirdyourdestiny@gmail.com>
2016-04-18 10:21:56 +0300

    blocks halfdone mainpage

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
 
           	<?php if ( have_rows('mainpage-block')) : ?>
           		<?php while( have_rows('mainpage-block')) : the_row(); ?>
-		            <div class="col-md-3 col-lg-3">
+		            <div class="col-md-3 col-lg-3 mainpage-block-area">
 		              <div class="mainpage-block">
 		                <div class="image-container">
 		                	<?php $image_attr = wp_get_attachment_image_src( get_sub_field('block-image') ); ?>
 		                  <img src="<?php echo $image_attr[0]; ?>" alt="">
 		                </div>
-		                <h2><?php the_sub_field('block-title'); ?></h2>
-		                <p><?php the_sub_field('block-text'); ?></p>
-		                <a href="" alt="">Learn more</a>
+		                <div class="text-container">
+			                <h3><?php the_sub_field('block-title'); ?></h3>
+			                <p><?php the_sub_field('block-text'); ?></p>
+			                <a href="" alt="">Learn more</a>
+			            </div>
 		              </div>
 		            </div>
             	<?php endwhile; ?>
