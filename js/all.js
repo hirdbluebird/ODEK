0ed56b27d68b8c0e6856cfd6390b7337b55ec430
hirdbluebird <bluebirdyourdestiny@gmail.com>
2016-04-19 09:55:09 +0300

    preapha slider

--- js/all.js
+++ js/all.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
 
-	$('.slider .triggers-wrapper .triggers li').on('click', function(event)) {
+	$('.slider .triggers-wrapper .triggers li').on('click', function(event) {
 		if(!$(event.target).is('active-slide')) {
 			$(this).addClass('active-slide').siblings().removeClass('active-tab');
 		}
-	}
+	});
 
 	var slideCount = $('.slider .images .slide').size(),
 		count = 0;
 
-	while(count < slideCount) {
+	while(count > slideCount) {
 		$('ul.triggers').append('<li>' + (count + 1) + '</li>');
 	}
 
-	function lightTrigger() {
+	function lightTrigger(index) {
 		$('ul.triggers li').removeClass('active-trigger').eq(index).addClass('active-trigger');
 	}
 
-	$('.slider .slide').first().addClass('active-slide');
+	$('.slider .images .slide').first().addClass('active-slide');
 	lightTrigger(0);
 
 	$('ul.triggers li').on('click', function() {
@@ -34,13 +34,15 @@ $(document).ready(function() {
 			$('.slider .images .active-slide').removeClass('active-slide').first().addClass('active-slide');
 			lightTrigger(0);
 		} else {
-			$('.slider .images .active-slide').removeclass('active-slide').next().addClass('active-slide');
-			lightTrigger(activeIndexSlide + 1);
+			$('.slider .images .active-slide').removeClass('active-slide').next().addClass('active-slide');
+			console.log(activeSlideIndex);
+			lightTrigger(activeSlideIndex + 1);
 		}
 	}
 
-	var sliderTimer;
 	function startSlide() {
 		setInterval(autoSlide, 1000);
 	}
+
+	startSlide();
 });
