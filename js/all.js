1fc403dec69332c97fbb3c49acf1e89d96f4b936
hirdbluebird <bluebirdyourdestiny@gmail.com>
2016-04-19 11:10:30 +0300

    mobile slider done alpha1.0

--- js/all.js
+++ js/all.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
 
-	$('.slider .triggers-wrapper .triggers li').on('click', function(event) {
-		if(!$(event.target).is('active-slide')) {
-			$(this).addClass('active-slide').siblings().removeClass('active-tab');
-		}
-	});
-
+	//counts slides number
 	var slideCount = $('.slider .images .slide').size(),
 		count = 0;
 
+	// adds trigger based on number of slides
 	while(count < slideCount) {
 		$('ul.triggers').append('<li></li>');
 		count++;
 	}
 
+	//lights active trigger
 	function lightTrigger(index) {
 		$('ul.triggers li').removeClass('active-trigger').eq(index).addClass('active-trigger');
 	}
-
+	//adds active class to first slide when page loaded
 	$('.slider .images .slide').first().addClass('active-slide');
 	lightTrigger(0);
 
+	//switch slide when trigger clicked
 	$('ul.triggers li').on('click', function() {
 		var triggerIndex = $(this).index();
 		lightTrigger(triggerIndex);
-		$('ul.triggers li').removeClass('active-trigger').index(triggerIndex).addClass('active-trigger');
+		$('.images .slide').removeClass('active-slide').eq(triggerIndex).addClass('active-slide');
 	});
 
+	//based on difference between whole number of slides and slide index, which count starts from zero
 	slideCount = slideCount - 1;
 
+	//automatically switch active slide on next
 	function autoSlide() {
 		var activeSlideIndex = $('.slider .images .active-slide').index();
 		if(activeSlideIndex === slideCount) {
@@ -43,6 +43,23 @@ $(document).ready(function() {
 	function startSlide() {
 		setInterval(autoSlide, 5000);
 	}
-
 	startSlide();
+
+	//activate slide, when trigger clicked
+	$('.slider .triggers-wrapper .triggers li').on('click', function(event) {
+		if(!$(event.target).is('active-slide')) {
+			$(this).addClass('active-slide').siblings().removeClass('active-tab');
+		}
+	});
+
+	//moves slider to mainpage
+	var window = $(window);
+
+	function checkWidth() {
+		var windowSize = window.width();
+		if(windowSize < 768) {
+			$('.slider').insertAfter('header');
+		}
+	}
+	checkWidth();
 });
